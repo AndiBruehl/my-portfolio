cbe9e3eaca90893afd574d2db709aef998cebd4f
Andi Brühl <a.bruehl2019@gmail.com>
2023-09-21 21:36:29 +0200

    bugfixing

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -5,38 +5,37 @@ import codeThinking from '../../img/codeThinking.svg'
 const listTitleStyle = { fontWeight: 900, color: '#9B1FE8', marginBotton: '4px'}
 
 const backendSkills =
-    <ul>
+    <ul key="backend">
         <li style={listTitleStyle}> BACKEND</li>
         <li>SQL - NoSQL - Python</li>
         <li>NodeJS</li>
     </ul>
 const frontendSkills =
-    <ul>
+    <ul key="frontend">
         <li style={listTitleStyle}> FRONTEND</li>
         <li>JavaScript - ReactJS - HTML&CSS</li>
         <li>Electron</li>
     </ul>
 const osSkills =
-    <ul>
+    <ul key="os">
         <li style={listTitleStyle}> OS</li>
         <li> Linux - Windows - Android</li>
     </ul>
 const cloudSkills =
-    <ul>
+    <ul key="cloud">
         <li style={listTitleStyle}> CLOUD</li>
         <li> AWS - GCP</li>
     </ul>
 const containerSkills =
-<ul>
-    <li style={listTitleStyle}> CONTAINER</li>
-    <li>Docker</li>
-</ul>
+    <ul key="container">
+        <li style={listTitleStyle}> CONTAINER</li>
+        <li>Docker</li>
+    </ul>
 const otherSkills =
-<ul>
-    <li style={listTitleStyle}> WEITERE SKILLS</li>
-    <li>Git - firebase - SCRUM</li>
-</ul>
-    ;
+    <ul key="others">
+        <li style={listTitleStyle}> WEITERE SKILLS</li>
+        <li>Git - firebase - SCRUM</li>
+    </ul>;
 
 const totalSkills = [backendSkills, frontendSkills, osSkills, cloudSkills, containerSkills, otherSkills];
 
@@ -52,9 +51,9 @@ const Skills = () => {
             </p>
             <div className={classes.Container}>
                 <img src={codeThinking} alt="CodeThinking" /><br/>
-                {totalSkills.map(skills => {
+                {totalSkills.map((skills, index) => {
                     return (
-                        <div className={classes.List}>
+                        <div className={classes.List} key={index}>
                             {skills}
                         </div>
                     )
