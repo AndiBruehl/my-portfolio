9ace818a61ed97d100b7dd5cbe7fc2e6f647fef3
AndiBruehl <a.bruehl2019@gmail.com>
2024-04-09 15:03:54 +0200

    Added new certs, refactored certs.js, added modal to portfolio

---
+++ src/components/Certs/ImageImport.jsx
@@ -0,0 +1,55 @@
+import slide_image_1 from "./assets/images/image1.png";
+import slide_image_2 from "./assets/images/image2.png";
+import slide_image_3 from "./assets/images/image3.png";
+import slide_image_4 from "./assets/images/image4.png";
+import slide_image_5 from "./assets/images/image5.png";
+import slide_image_6 from "./assets/images/image6.png";
+import slide_image_7 from "./assets/images/image7.png";
+import slide_image_8 from "./assets/images/image8.png";
+import slide_image_9 from "./assets/images/image9.png";
+import slide_image_10 from "./assets/images/image10.png";
+import slide_image_11 from "./assets/images/image11.png";
+import slide_image_12 from "./assets/images/image12.png";
+import slide_image_13 from "./assets/images/image13.png";
+import slide_image_14 from "./assets/images/image14.png";
+import slide_image_15 from "./assets/images/image15.png";
+import slide_image_16 from "./assets/images/image16.png";
+import slide_image_17 from "./assets/images/image17.png";
+import slide_image_18 from "./assets/images/image18.png";
+import slide_image_19 from "./assets/images/image19.png";
+import slide_image_20 from "./assets/images/image20.png";
+import slide_image_21 from "./assets/images/image21.png";
+import slide_image_22 from "./assets/images/image22.png";
+import slide_image_23 from "./assets/images/image23.png";
+import slide_image_24 from "./assets/images/image24.png";
+import slide_image_25 from "./assets/images/image25.png";
+
+const images = [
+  slide_image_1,
+  slide_image_2,
+  slide_image_3,
+  slide_image_4,
+  slide_image_5,
+  slide_image_6,
+  slide_image_10,
+  slide_image_11,
+  slide_image_12,
+  slide_image_13,
+  slide_image_14,
+  slide_image_15,
+  slide_image_21,
+  slide_image_17,
+  slide_image_16,
+  slide_image_20,
+  slide_image_18,
+  slide_image_22,
+  slide_image_19,
+  slide_image_7,
+  slide_image_8,
+  slide_image_9,
+  slide_image_23,
+  slide_image_24,
+  slide_image_25,
+];
+
+export default images;
