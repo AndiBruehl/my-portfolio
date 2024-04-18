0b0deaf1b5803346570727da255c71831b76b075
AndiBruehl <a.bruehl2019@gmail.com>
2024-04-18 19:36:23 +0200

    Added Number of visits

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
-import classes from './Contact.module.css';
-import { FaMailBulk, FaYoutube, FaGithub, FaLinkedin, FaTwitch } from 'react-icons/fa';
+import React, { useState } from "react";
+import classes from "./Contact.module.css";
+import {
+  FaMailBulk,
+  FaYoutube,
+  FaGithub,
+  FaLinkedin,
+  FaTwitch,
+} from "react-icons/fa";
 import { GrContact } from "react-icons/gr";
 import credly_white from "../../assets/credly_white.png";
-import ContactModal from './ContactModal';
-import { motion } from 'framer-motion'; // Import Framer Motion
+import ContactModal from "./ContactModal";
+import { motion } from "framer-motion"; // Import Framer Motion
+import VisitCounter from "../VisitCounter/VisitCounter";
 
 const Contacts = () => {
+  const trackingID = "G-N12S56676D";
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openContactForm = (e) => {
@@ -23,46 +31,74 @@ const Contacts = () => {
   const iconStyle = {
     margin: "1vw",
     marginTop: "1vh",
-    fontSize: '3vw', // Adjust the font size for the icons
+    fontSize: "3vw", // Adjust the font size for the icons
   };
 
   const credlyIconStyle = {
     margin: "1vw",
     marginTop: "1vh",
-    width: '6vw', // Adjust the width for the Credly icon
+    width: "6vw", // Adjust the width for the Credly icon
   };
 
   return (
-    <motion.div className={classes.Contact} id="contact"
+    <motion.div
+      className={classes.Contact}
+      id="contact"
       initial={{ opacity: 0 }} // Initial opacity set to 0
       animate={{ opacity: 1 }} // Animate opacity to 1
       exit={{ opacity: 0 }} // Animate opacity to 0 when modal is closed
     >
       <div className={classes.ContactIcons}>
         <div className="footer-icons">
-          <a href="https://github.com/AndiBruehl" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/AndiBruehl"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaGithub style={iconStyle} />
           </a>
-          <a href="https://www.linkedin.com/in/andreas-br%C3%BChl/" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.linkedin.com/in/andreas-br%C3%BChl/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaLinkedin style={iconStyle} />
           </a>
-          <a href="mailto:a.bruehl2019@gmail.com" target="_blank" rel="noreferrer">
+          <a
+            href="mailto:a.bruehl2019@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaMailBulk style={iconStyle} />
           </a>
-          <a href="https://www.credly.com/users/andreas-bruhl/badges" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.credly.com/users/andreas-bruhl/badges"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={credly_white} alt="Credly" style={credlyIconStyle} />
           </a>
-          <a href="https://www.youtube.com/@andreasbruehldev" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.youtube.com/@andreasbruehldev"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaYoutube style={iconStyle} />
           </a>
-          <a href="https://www.twitch.tv/tigersoul89" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.twitch.tv/tigersoul89"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaTwitch style={iconStyle} />
           </a>
-          <a href="#contact-form" onClick={openContactForm}> 
+          <a href="#contact-form" onClick={openContactForm}>
             <GrContact style={iconStyle} />
           </a>
         </div>
       </div>
+
+      <VisitCounter trackingID={trackingID} />
 
       <p className={classes.FooterText}>
         &copy; {currentYear} A. Brühl - All rights reserved
